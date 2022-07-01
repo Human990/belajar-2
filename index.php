47a03b4df4d1440c0d251f135b5cb06fde51f592
Human990 <singleuser69@gmail.com>
2022-07-01 13:51:35 +0700

    mengupdate data pada index.php

--- index.php
+++ index.php
@@ -9,5 +9,10 @@
 <body>
     <h1>Hello World! </h1>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum veritatis porro quae explicabo molestias cum iusto aliquid deleniti sed, architecto harum amet praesentium unde modi repellendus doloremque mollitia distinctio numquam!</p>
+    <ul>
+        <li>1</li>
+        <li>2</li>
+        <li>3</li>
+    </ul>
 </body>
 </html>
